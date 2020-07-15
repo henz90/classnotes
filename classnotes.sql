22eb678aba724d398e51551797a062c151fbf60d
henz90 <59123400+henz90@users.noreply.github.com>
2020-07-15 11:43:15 -0400

    Create classnotes.sql

---
+++ classnotes.sql
@@ -0,0 +1,232 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3333
+-- Generation Time: Jul 15, 2020 at 05:42 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `classnotes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `classid` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `semester` varchar(50) NOT NULL,
+  `year` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `commentid` int(11) NOT NULL,
+  `body` varchar(1000) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `userid` int(11) NOT NULL,
+  `filepathid` int(11) NOT NULL,
+  `level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `filepaths`
+--
+
+CREATE TABLE `filepaths` (
+  `filepathid` int(11) NOT NULL,
+  `filepath` varchar(100) NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lessons`
+--
+
+CREATE TABLE `lessons` (
+  `lessonid` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `body` varchar(10000) NOT NULL,
+  `classid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `filepathid` int(11) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `moderators`
+--
+
+CREATE TABLE `moderators` (
+  `userid` int(11) NOT NULL,
+  `classid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(320) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`classid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`commentid`),
+  ADD KEY `userid` (`userid`),
+  ADD KEY `filepathid` (`filepathid`);
+
+--
+-- Indexes for table `filepaths`
+--
+ALTER TABLE `filepaths`
+  ADD PRIMARY KEY (`filepathid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `lessons`
+--
+ALTER TABLE `lessons`
+  ADD PRIMARY KEY (`lessonid`),
+  ADD KEY `classid` (`classid`),
+  ADD KEY `userid` (`userid`),
+  ADD KEY `filepathid` (`filepathid`);
+
+--
+-- Indexes for table `moderators`
+--
+ALTER TABLE `moderators`
+  ADD PRIMARY KEY (`userid`,`classid`),
+  ADD KEY `classid` (`classid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `classid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `commentid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `filepaths`
+--
+ALTER TABLE `filepaths`
+  MODIFY `filepathid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lessons`
+--
+ALTER TABLE `lessons`
+  MODIFY `lessonid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `classes`
+--
+ALTER TABLE `classes`
+  ADD CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`filepathid`) REFERENCES `filepaths` (`filepathid`);
+
+--
+-- Constraints for table `filepaths`
+--
+ALTER TABLE `filepaths`
+  ADD CONSTRAINT `filepaths_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
+
+--
+-- Constraints for table `lessons`
+--
+ALTER TABLE `lessons`
+  ADD CONSTRAINT `lessons_ibfk_1` FOREIGN KEY (`classid`) REFERENCES `classes` (`classid`),
+  ADD CONSTRAINT `lessons_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `lessons_ibfk_3` FOREIGN KEY (`filepathid`) REFERENCES `filepaths` (`filepathid`);
+
+--
+-- Constraints for table `moderators`
+--
+ALTER TABLE `moderators`
+  ADD CONSTRAINT `moderators_ibfk_1` FOREIGN KEY (`classid`) REFERENCES `classes` (`classid`),
+  ADD CONSTRAINT `moderators_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
