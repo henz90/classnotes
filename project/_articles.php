bb470f15c62859fc248515d30f5d7ae14dbf5616
henz90 <59123400+henz90@users.noreply.github.com>
2020-07-22 14:18:53 -0400

    Update _articles.php

--- project/_articles.php
+++ project/_articles.php
@@ -36,7 +36,7 @@ $app->post('/create_class', function ($request, $response, $args) {
                 [ 'errorList' => $errorList, 'c' => ['name' => $name, 'body' => $body ]  ]);
     } else {
         $authorId = $_SESSION['user']['id'];
-        DB::insert('articles', ['userid' => $authorId, 'name' => $name, 'body' => $body]);
+        DB::insert('articles', ['userid' => $authorId, 'name' => $name, 'body' => $body, 'level' => 0]);
         $articleId = DB::insertId();
         return $this->view->render($response, 'addarticle_success.html.twig', ['id' => $articleId]);
     }
