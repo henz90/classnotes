e762b1ac04686e8555733c1413347f253a0e1d37
henz90 <59123400+henz90@users.noreply.github.com>
2020-07-22 10:15:36 -0400

    Updated some small changes

--- project/_main.php
+++ project/_main.php
@@ -2,23 +2,23 @@
 
 require_once '_setup.php';
 
+    //  MAINPAGE
+$app->get('/', function ($request, $response, $args) {
+        //return $response->write("This is Index");
+        $classesList = DB::query("SELECT c.classid, c.name, c.semester, c.year, c.userid, c.level"
+            . "FROM classes as c, users as u WHERE c.userid = u.userid ORDER BY c.classid DESC");
+        foreach ($classesList as &$article) {
+            // format posted date
+            $datetime = strtotime($article['creationTS']);
+            $postedDate = date('M d, Y \a\t H:i:s', $datetime );
+            $article['postedDate'] = $postedDate;
+        }
+        return $this->view->render($response, 'index.html.twig', ['list' => $classesList]);
+});
+
     //  INTERNAL ERROR
 $app->get('/internalerror', function ($request, $response, $args) {
     return $this->view->render($response, 'error_internal.html.twig');
-});
-
-    //  MAINPAGE
-$app->get('/', function ($request, $response, $args) {
-    //return $response->write("This is Index");
-    $classesList = DB::query("SELECT c.classid, c.name, c.semester, c.year, c.userid, c.level"
-        . "FROM classes as c, users as u WHERE c.userid = u.userid ORDER BY c.classid DESC");
-    foreach ($classesList as &$article) {
-        // format posted date
-        $datetime = strtotime($article['creationTS']);
-        $postedDate = date('M d, Y \a\t H:i:s', $datetime );
-        $article['postedDate'] = $postedDate;
-    }
-    return $this->view->render($response, 'index.html.twig', ['list' => $classesList]);
 });
 
     //  SESSION
