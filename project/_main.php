f754031b1549f766f04eb07e7d0acabc3d3b83d0
NalyaDoggo <norm_fong@hotmail.com>
2020-07-23 15:37:05 -0400

    fixed internalerror problem from main

--- project/_main.php
+++ project/_main.php
@@ -5,10 +5,13 @@ require_once '_setup.php';
     //  MAINPAGE
 $app->get('/', function ($request, $response, $args) {
         //return $response->write("This is Index");
-        $classesList = DB::query("SELECT c.classid, c.name, c.semester, c.year, c.userid, c.level, c.description"
-            . "FROM classes as c, users as u WHERE c.userid = u.userid ORDER BY c.classid DESC");
+        // TODO: Insert class description into the query
+        $classesList = DB::query("SELECT c.classid, c.name, c.semester, c.year, c.userid, c.level, u.name"
+        . " FROM classes as c, users as u WHERE c.userid = u.userid ORDER BY c.classid DESC");
+
             foreach ($classesList as &$article) {
-                $fullBodyNoTags = strip_tags($article['description']);
+                // changed from description to name for now. There's no description field in classes?
+                $fullBodyNoTags = strip_tags($article['name']);
                 $bodyPreview = substr(strip_tags($fullBodyNoTags), 0, 100);
                 $bodyPreview .= (strlen($fullBodyNoTags) > strlen($bodyPreview)) ? "..." : "";
                 $article['description'] = $bodyPreview;
