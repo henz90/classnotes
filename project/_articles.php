eff53f38a9ae67bb543e7f7b8429c3a9acc09f10
henz90 <59123400+henz90@users.noreply.github.com>
2020-07-23 21:10:28 -0400

    Correcting errors in creating a class

--- project/_articles.php
+++ project/_articles.php
@@ -19,7 +19,7 @@ $app->post('/create_class', function ($request, $response, $args) {
         return $this->view->render($response, 'error_access_denied.html.twig');
     }
     $name = $request->getParam('name');
-    $body = $request->getParam('description');
+    $body = $request->getParam('body');
     //  Sanitize the Body:
     $body = strip_tags($body, "<p><ul><li><em><strong><i><b><ol><h3><h4><h5><span>");
     $errorList = array();
