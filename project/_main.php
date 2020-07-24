1465ac7c63daad3e223576179b7813f0638ba234
henz90 <59123400+henz90@users.noreply.github.com>
2020-07-24 09:07:44 -0400

    Daily Scrum

--- project/_main.php
+++ project/_main.php
@@ -7,7 +7,6 @@ $app->get('/', function ($request, $response, $args) {
         //return $response->write("This is Index");
         $classesList = DB::query("SELECT c.classid, c.name, c.semester, c.year, c.userid, c.level, c.body, u.name"
         . " FROM classes as c, users as u WHERE c.userid = u.userid ORDER BY c.classid DESC");
-
             foreach ($classesList as &$article) {
                 $fullBodyNoTags = strip_tags($article['body']);
                 $bodyPreview = substr(strip_tags($fullBodyNoTags), 0, 100);
