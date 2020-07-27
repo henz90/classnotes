fb71b5382ff68fc2a0c73bf07188ffa8e2f8ee4e
NalyaDoggo <norm_fong@hotmail.com>
2020-07-27 16:10:39 -0400

    attempted to make create_lesson

--- project/_articles.php
+++ project/_articles.php
@@ -44,3 +44,24 @@ $app->post('/create_class', function ($request, $response, $args) {
         return $this->view->render($response, 'addclass_success.html.twig', ['id' => $articleId]);
     }
 });
+
+//ADD LESSON
+// STATE 1: first display
+$app->get('/create_lesson', function ($request, $response, $args) {
+    if (!isset($_SESSION['user'])) { // refuse if user not logged in
+        $response = $response->withStatus(403);
+        return $this->view->render($response, 'error_access_denied.html.twig');
+    }
+    return $this->view->render($response, 'create_lesson.html.twig');
+});
+
+// AJAX use, to populate year dropdown
+/*
+$app->get('/islessonyear', function ($request, $response, $args) {
+    $record = DB::query("SELECT distinct year FROM classes order by year desc");
+    $returnvalue = "";
+    while ($row = mysqli_fetch_array($record)) {
+        $returnvalue = $returnvalue . "<option value=\"$row['year'];></option>"
+    }
+}); 
+*/
