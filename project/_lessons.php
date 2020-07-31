efc0b7c6e3ad07bee0c586495184a52aa002485a
NalyaDoggo <norm_fong@hotmail.com>
2020-07-31 17:09:54 -0400

    Lessons partially functional

--- project/_lessons.php
+++ project/_lessons.php
@@ -14,8 +14,39 @@ $app->get('/create_lesson', function ($request, $response, $args) {
 
 //WIP
 // creating lesson AJAX
-$app->get('/cancreatelesson/[{classname}]', function ($request, $response, $args) {
-    $classname = isset($args['classname']) ? $args['classname'] : "";
-    $id = DB::queryFirstRow("SELECT classid FROM classes WHERE classname=%s", $classname);
+$app->get('/create_lesson/[{classid}]', function ($request, $response, $args) {
+    $classid = isset($args['classid']) ? $args['classid'] : "";
+    $id = DB::queryFirstRow("SELECT classid FROM classes WHERE classid=%s", $classid);
     return $this->view->render($response, 'create_lesson.html.twig', ['c' => $id]);
+});
+
+$app->post('/create_lesson', function ($request, $response, $args) {
+    if (!isset($_SESSION['user'])) { // refuse if user not logged in
+        $response = $response->withStatus(403);
+        return $this->view->render($response, 'error_access_denied.html.twig');
+    }
+    $classid = $request->getParam('classinputid');
+    $title = $request->getParam('title');
+    $body = $request->getParam('body');
+    //  Sanitize the Body:
+    $body = strip_tags($body, "<p><ul><li><em><strong><i><b><ol><h3><h4><h5><span>");
+    $errorList = array();
+    if (preg_match('/^[a-zA-Z0-9\ \\._\'"-]{2,100}$/', $title) != 1) { // Reg check on classname
+        array_push($errorList, "Title must be 2-100 characters long and consist of letters, digits, "
+            . "spaces, dots, underscores, apostrophies, or minus sign.");
+        // keep the title even if invalid
+    }
+    if (strlen($body) < 2 || strlen($body) > 1000) {
+        array_push($errorList, "Body must be 2-1000 characters long");
+        // keep the body even if invalid
+    }
+    
+    if ($errorList) {
+        return $this->view->render($response, 'create_class.html.twig',
+                [ 'errorList' => $errorList, 'c' => ['title' => $title, 'body' => $body ]  ]);
+    } else {
+        $authorId = $_SESSION['user']['userid'];
+        DB::insert('lessons', ['title' => $title, 'body' => $body, 'classid' => $classid, 'userid' => $authorId, 'level' => 0]);
+        return $this->view->render($response, 'addlesson_success.html.twig');
+    }
 });
